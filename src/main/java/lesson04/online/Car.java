1e8a7d4adefb80abacf6afbedf061372909bc098
DT5Gen <marinovod@gmail.com>
2021-04-24 23:38:47 +0300

    lesson04v.1.1

---
+++ src/main/java/lesson04/online/Car.java
@@ -0,0 +1,8 @@
+package lesson04.online;
+
+public class Car {
+    String model;
+    int countWheels;
+    String color;
+
+}
---
+++ src/main/java/lesson04/online/Main.java
@@ -0,0 +1,31 @@
+package lesson04.online;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Car car1 = new Car();
+        Car car2 = new Car();
+
+        car1.color = "Blueного ";
+        car1.countWheels = 4;
+        car1.model = "Nissan";
+
+        car2.color = "Болотного";
+        car2.model = "UAZ Diversant";
+        car2.countWheels = 6;
+
+        System.out.println();
+        System.out.println("Это что за покемон? - " + car1.model);
+        System.out.println("Колёс -  - " + car1.countWheels);
+        System.out.println(car1.color + " цвета." );
+
+        System.out.println();
+
+        System.out.println("Это что за покемон? - " + car2.model);
+        System.out.println("Колёс -  - " + car2.countWheels);
+        System.out.println(car2.color + " цвета." );
+    }
+
+
+}
